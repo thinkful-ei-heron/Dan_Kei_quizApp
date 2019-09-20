30905421c41005cd655ff8b2d4be17ee75d929e7
danWagar <dandwagar@gmail.com>
2019-09-20 14:01:11 -0700

    submitAnswer test

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 'use strict';
-$(startQuiz);
+$(main);
 
-function loadLanding(){
+function main() {
+  loadLanding();
+  startQuiz();
+  console.log('in main after startquiz');
+  submitAnswer();
+}
+
+function loadLanding() {
   $('.main').html(
     `<h3>Test your knowledge on NFL record holders!</h3>
     <button type="button" class="startButton">Start the quiz</button>`
   );
 }
 
-function startQuiz(){
-  loadLanding();
-  $('.startButton').click(function (){
+function startQuiz() {
+  $('.startButton').click(function() {
     console.log('click');
     generateQuestion();
   });
 }
 
-function generateQuestion(){
+function generateQuestion() {
   let html = renderQuestionHtml() + renderOptionsHtml() + '<button type="submit">Submit</button></form>';
   console.log(renderQuestionHtml());
   console.log(renderOptionsHtml());
   $('.main').html(html);
 }
 
-function renderQuestionHtml(){
+function renderQuestionHtml() {
   //this is the question text
   const questionText = STORE.questions[STORE.questionNumber].question;
   return `
-    <form>
+    <form id='answerForm'>
     <fieldset>
       <legend class="questionText">
         ${questionText}
       </legend>
     </fieldset>
-  `; 
+  `;
 }
 
-function renderOptionsHtml(){
+function renderOptionsHtml() {
   const optionsText = STORE.questions[STORE.questionNumber].options;
   let optionsHtml = '';
   let ans = 1;
@@ -50,4 +56,15 @@ function renderOptionsHtml(){
     ans++;
   });
   return optionsHtml;
+}
+
+function submitAnswer() {
+  console.log('In submitAnswer');
+  $('#answerForm').on('submit', event => {
+    console.log('Form submitted');
+    // this stops the default form submission behavior
+    event.preventDefault();
+    const text = $('input[ansGroup]:checked').val();
+    console.log(text);
+  });
 }
