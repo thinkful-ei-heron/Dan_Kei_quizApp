192362d4dccfb532227a82808f93ebd9359a4142
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-23 12:51:54 -0400

    Updated store

--- store.js
+++ store.js
@@ -12,6 +12,46 @@ const STORE = {
       question: 'What player set the record for the most receptions in one season with 143 catches?',
       options: ['Marvin Harrison', 'Wes Welker', 'Antonio Brown', 'Julio Jones'],
       answer: 'Marvin Harrison'
+    },
+    {
+      question: 'What player has the record for the most receptions in one game?',
+      options: ['Wes Welker', 'Ray Rice', 'Saquon Barkley', 'Brandon Marshall'],
+      answer: 'Brandon Marshall'
+    },
+    {
+      question: 'Who is the quarterback to most recently throw for 7 touchdowns in one game?',
+      options: ['Peyton Manning', 'Drew Brees', 'Nick Foles', 'Mitch Trubisky'],
+      answer: 'Nick Foles'
+    },
+    {
+      question: 'What quarterback has thrown the most career interceptions?',
+      options: ['Brett Favre', 'Peyton Manning', 'Drew Brees', 'Vinny Testaverde'],
+      answer: 'Brett Favre'
+    },
+    {
+      question: 'What quarterback has thrown the most pick-sixes in their career?',
+      options: ['Vinny Testaverde', 'Drew Brees', 'Peyton Manning', 'Brett Favre'],
+      answer: 'Brett Favre'
+    },
+    {
+      question: 'What wide receiver has the record for most receiving yards in a non-overtime game?',
+      options: ['Calvin Johnson', 'Antonio Brown', 'Flipper Anderson', 'Josh Gordon'],
+      answer: 'Calvin Johnson'
+    },
+    {
+      question: 'What defensive player has the record for the most sacks recorded in a single season?',
+      options: ['Reggie White', 'J.J. Watt', 'Lawrence Taylor', 'Michael Strahan'],
+      answer: 'Michael Strahan'
+    },
+    {
+      question: 'What player holds the record for most rushing attempts in a career?',
+      options: ['Walter Payton', 'Jerome Bettis', 'Emmitt Smith', 'Curtis Martin'],
+      answer: 'Emmitt Smith'
+    },
+    {
+      question: 'What player holds the record for the most interceptions in a single season?',
+      options: ['Mel Blount', 'Deion Sanders', 'Dick \'Night Train\' Lane', 'Jairus Byrd'],
+      answer: 'Dick \'Night Train\' Lane'
     }
   ]
 };
